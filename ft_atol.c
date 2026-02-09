bda628fa04eae966528e54723b57e411e5bd6976
Veronika Skopova <vskopova@c1r2s4.42prague.com>
2026-02-09 23:27:25 +0100

    .

--- ft_atol.c
+++ ft_atol.c
@@ -1,9 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atol.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vskopova <vskopova@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/02/09 23:14:02 by vskopova          #+#    #+#             */
+/*   Updated: 2026/02/09 23:15:20 by vskopova         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "push_swap.h"
 
 int	ft_atol(char *str, long *num)
 {
 	int		i;
-	int 	sign;
+	int		sign;
 	long	result;
 
 	result = 0;
@@ -12,14 +24,10 @@ int	ft_atol(char *str, long *num)
 	while (str[i] == ' ' || (str[i] >= 9 && str[i] < 13))
 		i++;
 	if (str[i] == '+' || str[i] == '-')
-	{
-		if (str[i] == '-')
+		if (str[i++] == '-')
 			sign *= -1;
-		i++;
-	}
 	if (str[i] < '0' || str[i] > '9')
 		return (0);
-
 	while (str[i])
 	{
 		if (str[i] < '0' || str[i] > '9')
@@ -30,6 +38,5 @@ int	ft_atol(char *str, long *num)
 		i++;
 	}
 	*num = result * sign;
-	return(1);
+	return (1);
 }
-
